0cce3af4fe866d1f1dba7fdc27df2b28ea285b9a
SVTRamiere <ramiere.benoit@orange.fr>
2025-07-13 18:48:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-var map = L.map('map').setView([48.3, -4.3], 6);
 
-var satellite = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
-}).addTo(map);
+document.addEventListener("DOMContentLoaded", function() {
+    const map = L.map('map').setView([48.5, -2.5], 6);
 
-// Couches météo (fictives, en attente de vraies URLs)
-var clouds = L.tileLayer('https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=0b9fa4a8f48537427496561d2fb532bb');
-var precip = L.tileLayer('https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=0b9fa4a8f48537427496561d2fb532bb');
-var temp = L.tileLayer('https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=0b9fa4a8f48537427496561d2fb532bb');
-var wind = L.tileLayer('https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=0b9fa4a8f48537427496561d2fb532bb');
+    const baselayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
 
-var baseMaps = { "Satellite": satellite };
-var overlayMaps = {
-    "Nuages": clouds,
-    "Précipitations": precip,
-    "Température": temp,
-    "Vent": wind
-};
+    const temp = L.tileLayer('https://tile.openweathermap.org/map/temp_new/{z}/{x}/{y}.png?appid=0b9fa4a8f48537427496561d2fb532bb');
+    const clouds = L.tileLayer('https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=0b9fa4a8f48537427496561d2fb532bb');
+    const precip = L.tileLayer('https://tile.openweathermap.org/map/precipitation_new/{z}/{x}/{y}.png?appid=0b9fa4a8f48537427496561d2fb532bb');
+    const wind = L.tileLayer('https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid=0b9fa4a8f48537427496561d2fb532bb');
 
-L.control.layers(baseMaps, overlayMaps).addTo(map);
+    const overlays = {
+        "Température": temp,
+        "Nuages": clouds,
+        "Précipitations": precip,
+        "Vent": wind
+    };
+
+    L.control.layers(null, overlays).addTo(map);
+
+    document.getElementById("zoom-bretagne").addEventListener("click", () => {
+        map.setView([48.4, -3.2], 8);
+    });
+
+    document.getElementById("capture").addEventListener("click", () => {
+        html2canvas(document.querySelector("#map")).then(canvas => {
+            const link = document.createElement("a");
+            link.download = "carte_meteo.png";
+            link.href = canvas.toDataURL();
+            link.click();
+        });
+    });
+});
